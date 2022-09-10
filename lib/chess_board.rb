adce2edc547ad04e74795dee0e34050081172e29
sinarin <alexngdragon@hotmail.com>
2022-09-10 03:33:47 -0400

    add more comments

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -22,7 +22,7 @@ Vector = Struct.new(:x, :y)
 
 
 class Knight < ChessBoard
-
+  #all the possible directions(vectors) a knight can move
   @@change_in_position = [Vector.new(2, 1), Vector.new(2, -1), Vector.new(-2, 1), Vector.new(-2, -1), Vector.new(1, 2), Vector.new(1, -2), Vector.new(-1, 2), Vector.new(-1, -2)]
   def initialize(board)
     #possible changes in position
@@ -30,12 +30,14 @@ class Knight < ChessBoard
     make_knight_graph(@chess_board)
   end
 
+  #create the edges between vertices
   def make_knight_graph(chess_board)
     @chess_board.each do |key, square|
       valid_move(@@change_in_position, square)
     end
   end
   
+  #checks if a move would go outside the boundries if not then creates the edge but adding to a list of adjacent nodes
   def valid_move(vectors, square)
     vectors.each do |vector|
       x_new = vector.x + square.value[0]
@@ -49,31 +51,20 @@ class Knight < ChessBoard
   def shortest_path(start, end_square, path = [end_square], queue = [], count = 0)
     #for each adjacent node (move you can do) go there
     @chess_board[start].adjacent_nodes.each do |move|
-      #for each move push it in to the queue
+      #base case, if you get to the end square end recursive loop
       if move.value == end_square
         queue = []
         path.unshift(start)
-        if count == 0
-          return path
-        else
-          return true
-        end
+        return path
       end
+      #for each move push it in to the queue
       queue << move.value
     end
-    count += 1
     found = shortest_path(queue.shift, end_square, path, queue, count) if queue.length > 0
-    #if the end has been found push the square where you came from to the path array
-    count -= 1
+    #this part of the code only activates if the node/square has been found.
+    #for this recusive call push start of the move if the starting square can get to the position previously recorded(adjacent node)
     path.unshift(start) if @chess_board[start].adjacent_nodes.any? { |node| node == @chess_board[path[0]]}
-    if count == 0
-      return path
-    end
-    if found == true
-      found
-    else
-      false
-    end
+    path
   end
 
 end
@@ -94,4 +85,4 @@ end
 board = ChessBoard.new()
 knight = Knight.new(board)
 
-p knight.shortest_path([0, 0], [1, 1])
+p knight.shortest_path([0, 0], [4, 6])
